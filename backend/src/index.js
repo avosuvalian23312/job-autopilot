a7bcf0c7086d08554ccc647a70656f7f7483fa8c
Avetis Suvalian <avosuvalian23312@gmail.com>
2026-02-06 18:11:47 -0600

    Fix PATCH route registration for updateJobStatus

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,23 +1,27 @@
 const { app } = require("@azure/functions");
+
 const { generateDocuments } = require("./functions/generateDocuments");
 const { listJobs } = require("./functions/listJobs");
 const { updateJobStatus } = require("./functions/updateJobStatus");
 
+// Generate docs
+app.http("generateDocuments", {
+  methods: ["POST"],
+  authLevel: "anonymous",
+  handler: generateDocuments
+});
+
+// List jobs
 app.http("listJobs", {
   methods: ["GET"],
   authLevel: "anonymous",
   handler: listJobs
 });
 
-app.http("generateDocuments", {
-  methods: ["POST"],
-  authLevel: "anonymous",
-  handler: generateDocuments
-})
+// Update job status (THIS WAS THE PROBLEM)
 app.http("updateJobStatus", {
   methods: ["PATCH"],
-  authLevel: "anonymous",
   route: "jobs/{id}/status",
+  authLevel: "anonymous",
   handler: updateJobStatus
 });
-;
