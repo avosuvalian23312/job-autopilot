2b2aa3feee1e9a875a6aec3db21563b02056b9aa
Avetis Suvalian <avosuvalian23312@gmail.com>
2026-02-09 17:06:38 -0600

    new

--- frontend/job-auto-pilot/src/pages/Setup.jsx
+++ frontend/job-auto-pilot/src/pages/Setup.jsx
@@ -69,6 +69,7 @@ async function uploadToSasUrl(uploadUrl, file) {
     method: "PUT",
     headers: {
       "x-ms-blob-type": "BlockBlob",
+      "x-ms-version": "2020-10-02",
       "Content-Type": file.type || "application/octet-stream",
     },
     body: file,
